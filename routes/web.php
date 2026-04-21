db4944d01f0e0e8c5bf32dd3b53a5494e5137c7b
NguyenHuyGiaToan <nhgtoan215@gmail.com>
2026-04-21 14:27:42 +0700

    Thiết lập ban đầu

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,22 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Home routes (đặt các route liên quan đến trang chủ ở đây)
 
 
+
+
+// Order routes (đặt các route liên quan đến đặt hàng, giỏ hàng ở đây)
+
+
+
+
+
+// Admin routes (đặt các route liên quan đến trang quản trị ở đây)
+
+
+
+
+
+//
 require __DIR__.'/auth.php';
