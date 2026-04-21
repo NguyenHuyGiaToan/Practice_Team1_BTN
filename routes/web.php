ad36583a0b0caba3b644b3d176e7760e12ef7596
NguyenHuyGiaToan <nhgtoan215@gmail.com>
2026-04-21 16:00:36 +0700

    Trang quản trị

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Home routes (đặt các route liên quan đến trang chủ ở đây)
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+// Home routes (đặt các route liên quan đến trang chủ ở đây)
+Route::get('/san-pham/{id}', [HomeController::class, 'show'])->name('sanpham.show');
 
 
 
@@ -21,10 +27,7 @@ Route::get('/dashboard', function () {
 
 
 // Admin routes (đặt các route liên quan đến trang quản trị ở đây)
-
-
-
-
-
+Route::delete('/laptop_delete/{id}', [App\Http\Controllers\AdminLaptopController::class, 'laptop_delete'])->name("laptop_delete");
+Route::get('/laptop_manager', [App\Http\Controllers\AdminLaptopController::class, 'laptop_manager'])->name("laptop_manager");
 //
 require __DIR__.'/auth.php';
